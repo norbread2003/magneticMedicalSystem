2606ed6e69f5344b238f00837083850fee0f8332
Yunlin Tan <tanyunlin2003@gmail.com>
2022-03-25 19:41:32 +0800

    Init Project

---
+++ app.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
+
+app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'index.html',
+        title='Home Page',
+        year=datetime.now().year,
+    )
+
+
+@app.route('/about')
+def about():
+    """Renders the about page."""
+    return render_template(
+        'about.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your application description page.'
+    )
+
+
+if __name__ == '__main__':
+    app.run()
